26df6fa1274d49c7d5c65c9f1c241da155885b89
Hsins <hsinspeng@gmail.com>
2019-06-25 19:40:10 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 #   git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # - if you are deploying to https://<USERNAME>.github.io/<REPO>
 #   git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f "https://${access_token}@github.com/Hsins/Udemy-Notes.git" master:gh-pages
+git push -f "https://${TRAVIS_TOKEN}@github.com/Hsins/Udemy-Notes.git" master:gh-pages
 
 cd -
